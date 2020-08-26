5162fa3884fdaced2b7c9ed8c94fca49a506c21e
hhovhann <haik.hovhanisyan@gmail.com>
2020-08-26 13:43:16 +0400

    Added observables.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Post } from './posts/model/post.model';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +6,9 @@ import { Post } from './posts/model/post.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  storedPosts: Post[] = [];
+  // storedPosts: Post[] = [];
 
-  onPostAdded(post){
-    this.storedPosts.push(post);
-  }
+  // onPostAdded(post){
+  //   this.storedPosts.push(post);
+  // }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,21 +1,27 @@
-import { Component, Input, Inject} from '@angular/core'
+import { Component, Input,  OnInit, OnDestroy} from '@angular/core'
+import  { Subscription } from 'rxjs'
 import { Post } from '../model/post.model';
-import { PostService } from '../service/posts.service';
+import { PostsService } from '../service/posts.service';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss'],
 })
 
-export class PostListComponent{
-  // the posts date can be called from findAll from server side
-  @Input() posts: Post []
-  // [
-      //  { title: 'First Post', content: 'This is the first post\'s content' },
-      //  { title: 'Second Post', content: 'This is the seconde post\'s content' },
-      //  { title: 'Third Post', content: 'This is the third post\'s content' },
-  // ];
-  constructor(public postService: PostService){
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post [];
+  private postSub: Subscription;
+
+  constructor(public postsService: PostsService){
+  }
+  ngOnDestroy(): void {
+    this.postSub.unsubscribe();
   }
 
+  ngOnInit(): void {
+    this.posts = this.postsService.getPosts()
+    this.postSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
 }
