1f0afe8a1a052d4e87b81a37310e0c9800be7597
hhovhann <haik.hovhanisyan@gmail.com>
2020-08-26 04:47:08 +0400

    Added toolbar and corrected the styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,24 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule, FormsModule, ReactiveFormsModule,
-    BrowserAnimationsModule, MatInputModule,
+    BrowserAnimationsModule, MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule,
     AppRoutingModule
   ],
   providers: [],
