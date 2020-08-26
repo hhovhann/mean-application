d66a8640800d6693100fa0d5cb889960b669c447
hhovhann <haik.hovhanisyan@gmail.com>
2020-08-26 05:28:41 +0400

    Added accordion representation of post lists.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,25 @@ import { MatInputModule } from '@angular/material/input'
 import { MatCardModule } from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
+    PostListComponent,
     HeaderComponent
   ],
   imports: [
     BrowserModule, FormsModule, ReactiveFormsModule,
-    BrowserAnimationsModule, MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule,
+    BrowserAnimationsModule, MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule, 
     AppRoutingModule
   ],
   providers: [],
